bec2ee745b2a058fceaccce6d21b8f84c88f898c
meiyu <yumei@aaxischina.com>
2021-07-28 18:01:38 +0800

    fead: add game component

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,48 +1,52 @@
-import React, { lazy } from "react";
-import { useRoutes } from "react-router-dom";
-import WrapperRouteComponent from "./WrapperRoute";
+import React, { lazy } from 'react';
+import type { PartialRouteObject } from 'react-router';
+import { useRoutes } from 'react-router-dom';
+import WrapperRouteComponent from './WrapperRoute';
 
-import type { PartialRouteObject } from "react-router";
-
-const LoginPage = lazy(() => import("../pages/login"));
-const LayoutPage = lazy(() => import("../pages/layout"));
-const HomePage = lazy(() => import("../pages/home-page"));
-const HooksExample = lazy(() => import("../pages/hooks-example"));
-const RecoilExample = lazy(() => import("../pages/recoil-example"));
-const NotFoundPage = lazy(() => import("../pages/404"));
+const LoginPage = lazy(() => import('../pages/login'));
+const LayoutPage = lazy(() => import('../pages/layout'));
+const HomePage = lazy(() => import('../pages/home-page'));
+const GamePage = lazy(() => import('../pages/game-page'));
+const HooksExample = lazy(() => import('../pages/hooks-example'));
+const RecoilExample = lazy(() => import('../pages/recoil-example'));
+const NotFoundPage = lazy(() => import('../pages/404'));
 
 const _RouteList: PartialRouteObject[] = [
-  {
-    path: "/login",
-    element: <WrapperRouteComponent element={<LoginPage />} />,
-  },
-  {
-    path: "/home-page",
-    element: <WrapperRouteComponent element={<HomePage />} />,
-  },
-  {
-    path: "/",
-    element: <WrapperRouteComponent element={<LayoutPage />} />,
-    children: [
-      {
-        path: "/hooks-example",
-        element: <WrapperRouteComponent element={<HooksExample />} />,
-      },
-      {
-        path: "/recoil-example",
-        element: <WrapperRouteComponent element={<RecoilExample />} />,
-      },
-      {
-        path: "*",
-        element: <WrapperRouteComponent element={<NotFoundPage />} />,
-      },
-    ],
-  },
+	{
+		path: '/login',
+		element: <WrapperRouteComponent element={<LoginPage />} />,
+	},
+	{
+		path: '/home-page',
+		element: <WrapperRouteComponent element={<HomePage />} />,
+	},
+	{
+		path: '/game-page',
+		element: <WrapperRouteComponent element={<GamePage />} />,
+	},
+	{
+		path: '/',
+		element: <WrapperRouteComponent element={<LayoutPage />} />,
+		children: [
+			{
+				path: '/hooks-example',
+				element: <WrapperRouteComponent element={<HooksExample />} />,
+			},
+			{
+				path: '/recoil-example',
+				element: <WrapperRouteComponent element={<RecoilExample />} />,
+			},
+			{
+				path: '*',
+				element: <WrapperRouteComponent element={<NotFoundPage />} />,
+			},
+		],
+	},
 ];
 
 const RenderRoute: React.FC = () => {
-  const element = useRoutes(_RouteList);
-  return element;
+	const element = useRoutes(_RouteList);
+	return element;
 };
 
 export default RenderRoute;
